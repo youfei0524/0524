5c75c3745a358efde888d2f680dda2eac7644f71
feixiang <2214358826@qq.com>
2022-12-15 17:22:01 +0800

    F

---
+++ honey-home-server/honey-home-server/api/models/ActivityLog.js
@@ -0,0 +1,37 @@
+/**
+ * ActivityLog.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    name: {
+      type: "string",
+      required: true, // 必填
+    },
+    remarks: {
+      type: "string",
+    },
+    activity: {
+      model: "Activity",
+    },
+    user: {
+      model: "User",
+    },
+  },
+  tableName: "activity_logs",
+  schema: true, //
+};
